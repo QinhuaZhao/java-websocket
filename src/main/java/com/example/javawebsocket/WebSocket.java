3b84e19f2d7b52f5861b17e14c6e4353f9681ab4
zz <zz>
2022-03-11 12:30:53 +0800

    Initial commit

---
+++ src/main/java/com/example/javawebsocket/WebSocket.java
@@ -0,0 +1,86 @@
+package com.example.javawebsocket;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Scope("prototype")
+@Component
+@ServerEndpoint(value = "/ws/user/{user}")
+public class WebSocket {
+
+    private static int onlineCount = 0;
+    private static Map<String, WebSocket> clients = new ConcurrentHashMap<String, WebSocket>();
+    private Session session;
+    private String username;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("websocket 加载");
+    }
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam(value = "user") String user) {
+        this.username = user;
+        this.session = session;
+        addOnlineCount();
+        clients.put(username, this);
+        System.out.println(username + "已连接");
+    }
+
+    @OnClose
+    public void onClose() throws IOException {
+        clients.remove(username);
+        subOnlineCount();
+    }
+
+    @OnMessage
+    public void onMessage(String message) throws IOException {
+        //Pod pod = SpringBeanUtil.getBean(K8sPod.class).readPod(clusterId, teamspace, name);
+        sendMessageAll(message);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    public void sendMessageTo(String message, String To) throws IOException {
+        // session.getBasicRemote().sendText(message);
+        //session.getAsyncRemote().sendText(message);
+        for (WebSocket item : clients.values()) {
+            if (item.username.equals(To) )
+                item.session.getAsyncRemote().sendText(message);
+        }
+    }
+
+    public void sendMessageAll(String message) throws IOException {
+        for (WebSocket item : clients.values()) {
+            item.session.getAsyncRemote().sendText(message);
+        }
+    }
+
+    public static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public static synchronized void addOnlineCount() {
+        WebSocket.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        WebSocket.onlineCount--;
+    }
+
+    public static synchronized Map<String, WebSocket> getClients() {
+        return clients;
+    }
+
+}
